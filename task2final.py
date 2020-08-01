7fc42ada70addbd5fa911284d02921ec9052725c
sai960 <52404517+sai960@users.noreply.github.com>
2020-08-01 23:27:03 +0530

    updated task2

--- task2final.py
+++ task2final.py
@@ -1,39 +1,27 @@
-
 def Email(list):
     dic={}
     for i in list:
         list1=i.split('@')
-        
         domain=list1[1]
         ename=list1[0]
         if(domain in dic):
             dic[domain].append(ename)
-        
-           
         else:
-            dic[domain]={}
             dic[domain]=[]
             dic[domain].append(ename)
-
     return dic
-def TotalNumOfEmployees(data):
     dic={}
     lst=data.keys()
     for i in list:
         dic[i]=len( TotalEmployees(data,i))    
     return dic
-
-list=['sai@google.com','ravi@amazon.com','sai@google.co.in','uday@amazon.in','kiran@salesforce.com','sunil@salesforce.com','maruthi@amazon.in','nawaz@google.io','madhu@microsoft.com','tharun@google.com']
-
+list=['sunil@dhruvsoft.co.in','venky@dhruvsoft.in','sai@dhruvsoft.com','roshini@google.com','ajay@google.com']
 data=Email(list)
 i=0
 for domain in data:
-    
     print("The number of employees in the domain -",domain)
-    
     for emp in data[domain]:
         i+=1
     print(i)
     i=0
-        
 print(data)
